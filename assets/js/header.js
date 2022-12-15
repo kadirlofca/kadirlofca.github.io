6ad8591923eefb43f556ef2b264ce80e26168786
Kadir Lofca <kadirlofca@outlook.com>
2022-12-15 12:57:20 -0500

    magnolia added but inactive

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,10 +3,12 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
     document.getElementById("header-wave").style.height = "0";
-    document.getElementById("header-wave").style.marginBottom = "75px";
+    document.getElementById("header-wave").style.paddingBottom = "75px";
+    document.getElementsByTagName("header")[0].style.height = "50px";
   } else {
     document.getElementById("header-wave").style.height = "75px";
-    document.getElementById("header-wave").style.marginBottom = "0";
+    document.getElementById("header-wave").style.paddingBottom = "0";
+    document.getElementsByTagName("header")[0].style.height = "110px";
 
   }
 }   
